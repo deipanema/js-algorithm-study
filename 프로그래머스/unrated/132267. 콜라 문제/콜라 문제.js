adbad5def0ec15064afd35f775caf7fc43f5c136
deipanema <97335934+deipanema@users.noreply.github.com>
2023-04-28 10:21:58 +0900

    Update 콜라 문제.js

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,16 +1,8 @@
 function solution(a, b, n) {
     let answer = 0;
     let keep = 0;
-//a가 제수(divisor)
-//b는 마트가 주는 병
-//n이 피제수(dividend)    
-// 20 /2 = 10 * b
-// 10 /2 = 5
-// 5 / 2 = 2
-// 2 / 2 = 1
-// cnt = 19
     
-    while(a < n){
+    while(a <= n){
        if(n % a) {
            keep = (n % a);
            n -= keep;
@@ -23,6 +15,5 @@ function solution(a, b, n) {
     
     }
     
-    return n == a ? answer + ((n / a) * b) : answer
+    return answer;
 }
-

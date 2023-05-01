a305cf1e9d6c64cac102bb402bba5ab562bb3954
deipanema <97335934+deipanema@users.noreply.github.com>
2023-05-01 13:23:16 +0900

    Update ［1차］ 비밀지도.js
    2진수 OR 연산하는 부분 수정

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -5,17 +5,10 @@
 function solution(n, arr1, arr2) {
     const answer = [];
     
-    // 2진수 아이템을 갖는 배열로 바꿔주기
-    const arr_1 = arr1.map(v => v.toString(2)); 
-    const arr_2 = arr2.map(v => v.toString(2));
-    
-    
     for(let i =0; i < n; i++){
-        answer.push((parseInt(arr_1[i],2) | parseInt(arr_2[i],2)).toString(2)+"");
+        answer.push((arr1[i] | arr2[i]).toString(2));
         if(n != answer[i].length) answer[i] = '0'.repeat(n - answer[i].length) + answer[i];
     }
-    
-    console.log('answer',answer)
     
     let point = 0;
     let result = Array.from({length: n}, () => '');

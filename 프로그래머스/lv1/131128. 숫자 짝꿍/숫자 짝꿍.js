c26c5d3635b4e3cde2338b6b56801aabb8f3c384
deipanema <97335934+deipanema@users.noreply.github.com>
2023-05-04 09:58:25 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -0,0 +1,17 @@
+function solution(X, Y) {
+    const obj_X = {};
+    let answer = [];
+    
+    for(const o of X){
+        obj_X[o] = (obj_X[o] || 0) + 1;
+    }
+    
+    for(const v of Y){
+        if(obj_X[v]){
+            answer.push(v);
+            obj_X[v]--;
+        }
+    }
+    
+    return !answer.length ? "-1" : answer[0] === "0" ? "0" : answer.sort((a,b) => b-a).join("");
+}

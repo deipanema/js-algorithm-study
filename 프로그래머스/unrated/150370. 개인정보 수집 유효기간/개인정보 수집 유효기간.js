d0c5059802c7c15b481e9ccbb4bff06f65571615
deipanema <97335934+deipanema@users.noreply.github.com>
2023-05-17 11:02:58 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 6.21 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,42 @@
+function solution(today, terms, privacies) {
+    const answer = [];
+    const limits = [];
+    const exDates = new Map();
+    
+    for(const term of terms){
+        let [kind, period] = term.split(" ");
+        exDates.set(kind, +period);
+    }
+    
+    for(let i = 0; i < privacies.length; i++){
+        let [period, kind] = privacies[i].split(" ");
+        let [year, month, day] = period.split(".").map(Number);
+        month = month + exDates.get(kind);
+        day = day - 1;
+        
+        
+        
+        while(12 < month){
+            month = month - 12
+            year = year + 1
+        }
+        
+        
+        if(day < 1){
+            day = 28 + day;
+            month = month - 1;
+        }
+        
+        
+        
+        month = (month+'').padStart(2, 0);
+        day = (day+'').padStart(2, 0);
+        
+        if(+`${year}${month}${day}` < +today.replaceAll('.','')) answer.push(i+1);
+        console.log(+`${year}${month}${day}`)
+        console.log(+today.replaceAll('.',''))
+    }
+    
+    
+    return answer;
+}

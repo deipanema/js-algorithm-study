1128fea3b7864cf04ee8b3f8583c4eceb1769029
deipanema <97335934+deipanema@users.noreply.github.com>
2023-05-17 13:37:48 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.57 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,7 +1,8 @@
 function solution(today, terms, privacies) {
     const answer = [];
-    const limits = [];
     const exDates = new Map();
+    let [year, month, day] = today.split('.').map(Number);
+    const todaySum = (year * 28 * 12) + (month * 28) + day;
     
     for(const term of terms){
         let [kind, period] = term.split(" ");
@@ -11,26 +12,10 @@ function solution(today, terms, privacies) {
     for(let i = 0; i < privacies.length; i++){
         let [period, kind] = privacies[i].split(" ");
         let [year, month, day] = period.split(".").map(Number);
-        month = month + exDates.get(kind);
-        day = day - 1;
+        const exSum = (year * 28 * 12) + ((month + exDates.get(kind)) * 28) + day;
         
-        
-        while(12 < month){
-            month = month - 12
-            year = year + 1
-        }
-        
-        if(day < 1){
-            day = 28 + day;
-            month = month - 1;
-        }
-        
-        
-        
-        month = (month+'').padStart(2, 0);
-        day = (day+'').padStart(2, 0);
-        
-        if(+`${year}${month}${day}` < +today.replaceAll('.','')) answer.push(i+1);
+         if(exSum <= todaySum) answer.push(i+1);
+       
     }
     
     
